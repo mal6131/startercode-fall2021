ad9fc3dffd01a1eca94bbf714e00638deab599e1
mal6131 <89483023+mal6131@users.noreply.github.com>
2021-09-27 12:01:16 -0400

    Update hw1.py

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,21 +1,47 @@
 from hwfunctions import fun_factor, fun_inc
 
-
 def delayed_increment(c, start, end):
     # use fun_inc
-    pass
+    # return  sum([ fun_inc(i) for i in  range(start , end)]
+    objects = []
 
+    for i in range(start, end):
+        x = delayed(fun_inc)(i)
+        objects.append(x)
+
+    z = delayed(sum)(objects)
+    return z
 
 def delayed_factor(c, start, end):
     # use fun_factor
-    pass
+    # return  sum([ fun_factor(i) for i in range(start , end)])
+    objects = []
 
+    for i in range(start, end):
+        x = delayed(fun_factor)(i)
+        objects.append(x)
+
+    z = delayed(sum)(objects)
+    return z
 
 def future_increment(c, start, end):
     # use fun_inc
-    pass
+    objects = []
 
+    for i in range(start,end):
+        x = c.submit(fun_inc, i)
+        objects.append(x)
+
+    z = c.submit(sum, objects)
+    return z
 
 def future_factor(c, start, end):
     # use fun_factor
-    pass
+    objects = []
+
+    for i in range(start, end):
+        x = c.submit(fun_factor, i)
+        objects.append(x)
+
+    z = c.submit(sum, objects)
+    return z
